ad2fa259a9991980c03de98564fa446247e37d3c
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2023-11-14 12:29:38 +0530

    Add files via upload

---
+++ Day 20 and 21/snek_logic.py
@@ -0,0 +1,54 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.sneks = []
+        self.create_snake()
+        self.head = self.sneks[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_snek_body = Turtle("square")
+        new_snek_body.penup()
+        new_snek_body.color("white")
+        new_snek_body.setpos(position)
+        self.sneks.append(new_snek_body)
+
+    def extend(self):
+        self.add_segment(self.sneks[-1].position())
+
+    def move(self):
+        for snek_num in range(len(self.sneks) - 1, 0, -1):
+            # setting new coordinates for the body parts
+            new_x = self.sneks[snek_num - 1].xcor()
+            new_y = self.sneks[snek_num - 1].ycor()
+            # assigning the new coordinates to the part
+            self.sneks[snek_num].setpos(new_x, new_y)
+        self.sneks[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
