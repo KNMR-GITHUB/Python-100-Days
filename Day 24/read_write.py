154d210eab32a633818104a86fc6494fca31b27b
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2023-11-17 12:39:56 +0530

    Add files via upload

---
+++ Day 24/read_write.py
@@ -0,0 +1,24 @@
+# file = open('data.txt', )
+# f1 = file.read(
+# print(f1)
+# file.close()
+
+# ez way to open files
+# and append to that file
+
+# with open("data.txt", mode="a") as file:
+#     file.write("""
+# And I scream at the top of my lungs
+#
+# What's going on?
+# And I say, HEYYEYAAEYAAAEYAEYAA
+# HEYYEYAAEYAAAEYAEYAA
+# I said hey, what's going on?
+# And i say, HEYYEYAAEYAAAEYAEYAA
+# HEYYEYAAEYAAAEYAEYAA
+# i said hey,whats going on """)
+
+# writing on a file, if it exist write else create n write
+#
+# with open("heyo.txt", mode="w") as heyo:
+#     heyo.write("HEYO !")
